efd1e988c77013c0c96536676ab27dd0b7539973
appdeve-123 <105652514+appdeve-123@users.noreply.github.com>
2022-05-16 16:25:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+streamlit.title('My parents new breakfast menu')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
